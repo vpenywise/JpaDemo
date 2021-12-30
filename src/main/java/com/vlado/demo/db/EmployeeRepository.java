c0fed7068875a44d6ca4b8f86a20b0a06c320700
Hristo Krachunov <hristo.krachunov@alpiq.com>
2021-12-30 11:23:32 +0200

    Add additional query methods

--- src/main/java/com/vlado/demo/db/EmployeeRepository.java
+++ src/main/java/com/vlado/demo/db/EmployeeRepository.java
@@ -2,5 +2,16 @@ package com.vlado.demo.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    public Employee getEmployeeByName(String name);
+
+    public Employee getEmployeeByDept(String dept);
+
+    public List<Employee> findAllByDept(String dept);
+
+    public List<Employee> findAllByDeptAndName(String dept, String name);
+
 }
--- src/test/java/com/vlado/demo/DemoApplicationTests.java
+++ src/test/java/com/vlado/demo/DemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.vlado.demo;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
+import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +22,28 @@ class DemoApplicationTests {
     void contextLoads() {
         Employee employee = new Employee();
         employee.setDept("dept");
-        employee.setName("GoshYtu");
+        employee.setName("GoshYtu663334");
         employee.setSalary(100);
-        employee.setId(1L);
+
+        long leftLimit = 1L;
+        long rightLimit = 10L;
+        long generatedLong = leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
+
+        employee.setId(generatedLong);
         Employee save = employeeRepository.save(employee);
+
+        Employee goshYtu = employeeRepository.getEmployeeByName("GoshYtu663334");
+        assertNotNull(goshYtu);
         assertNotNull(save);
+
+        List<Employee> dept = employeeRepository.findAllByDept("dept");
+        assertNotNull(dept);
+
+        List<Employee> dept2 = employeeRepository.findAllByDeptAndName("dept", "GoshYtu663334");
+        assertNotNull(dept2);
+
     }
-    
+
     @Test
     void getAllEmp() {
         List<Employee> all = employeeRepository.findAll();
